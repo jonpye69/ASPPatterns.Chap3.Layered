2f37afaf0e648ec5a54adb679893b6afae50d211
jonpye <jon.pye@nbt.nhs.uk>
2017-10-04 15:52:07 +0100

    Add project files.

---
+++ ASPPatterns.Chap3.Layered/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ASPPatterns.Chap3.Layered.Startup))]
+namespace ASPPatterns.Chap3.Layered
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
